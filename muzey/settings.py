17742b75c6c69c0ac676b27c7a2e387661edd9f3
Rok <rklancar@gmail.com>
2019-06-11 23:23:38 +0200

    modified settings.py for staticfiles

--- muzey/settings.py
+++ muzey/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'website/media')
 MEDIA_URL = "/media/"
 AWS_ACCESS_KEY_ID= os.environ.get('AWS_ACCESS_KEY_ID')
@@ -131,5 +131,9 @@ AWS_STORAGE_BUCKET_NAME= os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 django_heroku.settings(locals())
